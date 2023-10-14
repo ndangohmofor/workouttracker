71be0d1312d4ffb96840c088268926f3486686b8
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-10-14 13:33:50 -0400

    Updated the user repository to find users by role

--- src/main/java/com/meufty/workoutplanner/repository/UserRepository.java
+++ src/main/java/com/meufty/workoutplanner/repository/UserRepository.java
@@ -1,12 +1,14 @@
 package com.meufty.workoutplanner.repository;
 
 import com.meufty.workoutplanner.model.MyUser;
+import com.meufty.workoutplanner.model.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -20,4 +22,7 @@ public interface UserRepository extends JpaRepository<MyUser, Long> {
     @Query("UPDATE MyUser a " +
     "SET a.active = true WHERE a.username = ?1")
     int enableUser(String username);
+
+    @Query("SELECT mu.username, mu.email, mu.enabled, mu.locked FROM MyUser mu WHERE mu.userRole = ?1")
+    Optional<List<MyUser>> findMyUserByRole(Enum<UserRole> userRole);
 }

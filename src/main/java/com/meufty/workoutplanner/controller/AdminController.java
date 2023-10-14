3f7b016068854559f460c14e9ea9e3169d097c55
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-10-14 13:34:17 -0400

    Added an admin controller to be able to retrieve users from the front end by role

--- src/main/java/com/meufty/workoutplanner/controller/AdminController.java
+++ src/main/java/com/meufty/workoutplanner/controller/AdminController.java
@@ -1,2 +1,28 @@
-package com.meufty.workoutplanner.controller;public class AdminController {
+package com.meufty.workoutplanner.controller;
+
+import com.meufty.workoutplanner.model.MyUser;
+import com.meufty.workoutplanner.model.UserRole;
+import com.meufty.workoutplanner.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@AllArgsConstructor
+@RequestMapping("/admin")
+public class AdminController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("gymusers")
+    public List<MyUser> getGymUsers(UserRole role) throws Exception{
+        return userRepository.findMyUserByRole(role).orElseThrow();
+    }
 }

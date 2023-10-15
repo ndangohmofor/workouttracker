1e4331b092dd6dcc9ec0b13925b38111b8cc1863
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-10-14 20:11:08 -0400

    Added the Valid annotation to the parameter list of the createAuthToken and changed the return for the bad credential to unauthorized

--- src/main/java/com/meufty/workoutplanner/controller/AuthenticationController.java
+++ src/main/java/com/meufty/workoutplanner/controller/AuthenticationController.java
@@ -9,12 +9,15 @@ import com.meufty.workoutplanner.service.MyUserDetailsService;
 import com.meufty.workoutplanner.util.JwtUtil;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @CrossOrigin
 @RestController
@@ -28,11 +31,12 @@ public class AuthenticationController {
     private UserRepository userRepository;
 
     @PostMapping(path = "/login")
-    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws BadCredentialsException {
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody @Valid AuthenticationRequest authenticationRequest) throws BadCredentialsException {
         try{
             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword()));
         } catch (BadCredentialsException e){
-            throw new BadCredentialsException("Incorrect Username or Password", e);
+//            throw new BadCredentialsException("Incorrect Username or Password", e);
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
         final UserDetails userDetails = myUserDetailsService.loadUserByUsername(authenticationRequest.getUsername());
         final String jwt = jwtTokenUtil.generateToken(userDetails);

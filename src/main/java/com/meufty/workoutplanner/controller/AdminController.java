f91a9a6606a5f9936196e03f91a83a32b31586cb
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-10-14 20:11:53 -0400

    Added an admin controller for performing admin functions. Currently, exposing a method for searching for users based on their roles

--- src/main/java/com/meufty/workoutplanner/controller/AdminController.java
+++ src/main/java/com/meufty/workoutplanner/controller/AdminController.java
@@ -15,7 +15,7 @@ import java.util.List;
 @RestController
 @CrossOrigin
 @AllArgsConstructor
-@RequestMapping("/admin")
+@RequestMapping("/api/v1/admin")
 public class AdminController {
 
     @Autowired

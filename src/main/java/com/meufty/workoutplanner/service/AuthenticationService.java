a82fedd7b26f33aee0ad854ff599ac6e8a1ab286
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-10-16 23:03:12 -0400

    Added an authentication service to do the leg work of all the authentication to free up the controller

--- src/main/java/com/meufty/workoutplanner/service/AuthenticationService.java
+++ src/main/java/com/meufty/workoutplanner/service/AuthenticationService.java
@@ -1,2 +1,50 @@
-package com.meufty.workoutplanner.service;public class AuthenticationService {
+package com.meufty.workoutplanner.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.meufty.workoutplanner.model.AuthenticationResponse;
+import com.meufty.workoutplanner.model.MyUser;
+import com.meufty.workoutplanner.model.MyUserDetails;
+import com.meufty.workoutplanner.repository.UserRepository;
+import com.meufty.workoutplanner.util.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+    private final UserRepository respository;
+    private final MyUserDetailsService myUserDetailsService;
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        final String refreshToken;
+        final String email;
+        MyUser myUser = null;
+        if (authHeader == null ||!authHeader.startsWith("Bearer ")){
+            return;
+        }
+        refreshToken = authHeader.substring(7);
+        String username = jwtUtil.extractUsername(refreshToken);
+        if (username != null){
+            myUser = respository.findByUsername(username)
+                    .orElseThrow();
+        }
+
+        UserDetails userDetails = myUserDetailsService.loadUserByUsername(myUser.getUsername());
+
+        if (jwtUtil.validateToken(refreshToken, userDetails)){
+            var accessToken = jwtUtil.generateToken(userDetails);
+            var authResponse = new AuthenticationResponse(accessToken, refreshToken, myUser.getUserRole());
+            new ObjectMapper().writeValue(response.getOutputStream(), authResponse);
+        }
+    }
 }

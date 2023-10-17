5d0db68301d7d34369ca9fa39e7d421d9a872759
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-10-16 22:05:43 -0400

    Updated the authentication response to include a refresh token. Also added json properties to annocate the access and refresh tokens

--- src/main/java/com/meufty/workoutplanner/model/AuthenticationResponse.java
+++ src/main/java/com/meufty/workoutplanner/model/AuthenticationResponse.java
@@ -1,11 +1,15 @@
 package com.meufty.workoutplanner.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
 public class AuthenticationResponse {
-    private String jwt;
+    @JsonProperty("access_token")
+    private String jwtLoginToken;
+    @JsonProperty("refresh_token")
+    private String jwtRefreshToken;
     private UserRole role;
 }

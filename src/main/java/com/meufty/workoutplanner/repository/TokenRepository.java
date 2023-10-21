60101c3997a1e34179d18c4b280bc83f6d2f288d
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-10-21 10:29:53 -0400

    added method to retrieve only the valid access tokens from the db

--- src/main/java/com/meufty/workoutplanner/repository/TokenRepository.java
+++ src/main/java/com/meufty/workoutplanner/repository/TokenRepository.java
@@ -1,6 +1,7 @@
 package com.meufty.workoutplanner.repository;
 
 import com.meufty.workoutplanner.model.Token;
+import com.meufty.workoutplanner.model.TokenType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -13,6 +14,12 @@ public interface TokenRepository extends JpaRepository<Token, Integer> {
             where u.id = :userId and (t.expired = false or t.revoked = false )
             """)
     List<Token> findAllValidTokensByUser(Long userId);
+
+    @Query("""
+select t from Token  t inner join MyUser u on t.user.id = u.id
+where u.id = :userId and t.tokenType = "BEARER" and (t.expired = false or t.revoked = false )
+""")
+    List<Token> findAllValidAccessTokensByUser(Long userId);
 
     Optional<Token> findByToken(String token);
 }

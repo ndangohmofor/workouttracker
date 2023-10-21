fdecb9b859fbeb0e615b2f82f5a5dd40e56fe0e2
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-10-20 23:37:55 -0400

    Added a logout service handler to handle logout

---
+++ src/main/java/com/meufty/workoutplanner/service/LogoutService.java
@@ -0,0 +1,37 @@
+package com.meufty.workoutplanner.service;
+
+import com.meufty.workoutplanner.repository.TokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Service
+@RequiredArgsConstructor
+public class LogoutService implements LogoutHandler {
+
+    @Autowired
+    private TokenRepository tokenRepository;
+    @Override
+    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        final String authorizationHeader = request.getHeader("Authorization");
+
+        String username = null;
+        String jwt = null;
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            jwt = authorizationHeader.substring(7);
+        }
+
+        var storedToken = tokenRepository.findByToken(jwt).orElse(null);
+        if (storedToken != null){
+            storedToken.setRevoked(true);
+            storedToken.setRevoked(true);
+            tokenRepository.save(storedToken);
+        }
+    }
+}

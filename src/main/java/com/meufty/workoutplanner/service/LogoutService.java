a61e9bbc77473cfb0046b0c3a0f05a80f74ee6d9
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-11-04 15:32:48 -0400

    extracted method to delete user tokens out to the jwtutil class. optimized fetching the cookie from the request. instead of creating a new token with the name, just set the maxAge to 0 and the value to null on the same token

--- src/main/java/com/meufty/workoutplanner/service/LogoutService.java
+++ src/main/java/com/meufty/workoutplanner/service/LogoutService.java
@@ -1,70 +1,40 @@
 package com.meufty.workoutplanner.service;
 
-import com.meufty.workoutplanner.model.MyUser;
-import com.meufty.workoutplanner.model.MyUserDetails;
-import com.meufty.workoutplanner.model.Token;
 import com.meufty.workoutplanner.repository.TokenRepository;
 import com.meufty.workoutplanner.repository.UserRepository;
 import com.meufty.workoutplanner.util.JwtUtil;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.token.TokenService;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.CookieValue;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class LogoutService implements LogoutHandler {
 
-    @Autowired
-    private TokenRepository tokenRepository;
-    @Autowired
-    private JwtUtil jwtUtil;
-    @Autowired
-    private UserRepository userRepository;
+    public TokenRepository tokenRepository;
+    public JwtUtil jwtUtil;
+    public UserRepository userRepository;
     @Override
     public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-        List<Cookie> cookies = Arrays.stream(request.getCookies()).filter(cookie -> cookie.getName().equals("refreshToken")).collect(Collectors.toList());
+        Cookie refreshTokenCookie = Arrays.stream(request.getCookies()).filter(cookie -> cookie.getName().equals("refreshToken")).collect(Collectors.toList()).get(0);
 
-        String refreshToken = null;
-        if (!cookies.isEmpty()){
-            refreshToken = cookies.get(0).getValue();
-        }
+        var refreshToken = refreshTokenCookie.getValue();
 
-        var storeRefreshToken = tokenRepository.findByToken(refreshToken).orElse(null);
-        if (storeRefreshToken != null){
-            storeRefreshToken.setRevoked(true);
-            storeRefreshToken.setExpired(true);
-            tokenRepository.delete(storeRefreshToken);
-        }
+        refreshTokenCookie.setMaxAge(0);
+        refreshTokenCookie.setValue(null);
 
-        var username = jwtUtil.extractUsername(refreshToken);
-        MyUser myUser = userRepository.findByUsername(username).orElse(null);
-        assert myUser != null;
-        List<Token> tokens = tokenRepository.findAllByUser(myUser);
+        jwtUtil.deleteAllUserTokens(refreshToken);
 
-        //Revoke, expire and delete all jwt tokens the current belong to the user
-        tokens.forEach(token -> {
-            token.setExpired(true);
-            token.setRevoked(true);
-            tokenRepository.delete(token);
-        });
-
-        ResponseCookie deleteRefreshTokenCookie = ResponseCookie.from("refreshToken", null)
-                .build();
-
-        ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, deleteRefreshTokenCookie.toString()).build();
+        ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString()).build();
     }
+
 }

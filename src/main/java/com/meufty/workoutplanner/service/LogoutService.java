38f5b139cc9091319f8453301ff32d5fa579568a
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-11-03 21:45:35 -0400

    Updated the logout service to delete the expire, revoke and delete the stored refresh token from the db when a user logs out

--- src/main/java/com/meufty/workoutplanner/service/LogoutService.java
+++ src/main/java/com/meufty/workoutplanner/service/LogoutService.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CookieValue;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
 
 @Service
 @RequiredArgsConstructor
@@ -19,8 +22,13 @@ public class LogoutService implements LogoutHandler {
     @Override
     public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
         final String authorizationHeader = request.getHeader("Authorization");
+        Cookie[] cookies = request.getCookies();
 
         String jwt = null;
+        String refreshToken = null;
+        if (cookies.length > 0){
+            refreshToken = extractRefreshTokenFromCookie(cookies[0].toString());
+        }
 
         if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
             jwt = authorizationHeader.substring(7);
@@ -32,5 +40,16 @@ public class LogoutService implements LogoutHandler {
             storedToken.setExpired(true);
             tokenRepository.delete(storedToken);
         }
+
+        var storeRefreshToken = tokenRepository.findByToken(refreshToken).orElse(null);
+        if (storeRefreshToken != null){
+            storeRefreshToken.setRevoked(true);
+            storeRefreshToken.setExpired(true);
+            tokenRepository.delete(storeRefreshToken);
+        }
+    }
+    
+    public String extractRefreshTokenFromCookie(@CookieValue(name = "refreshToken") String refreshToken){
+        return refreshToken;
     }
 }

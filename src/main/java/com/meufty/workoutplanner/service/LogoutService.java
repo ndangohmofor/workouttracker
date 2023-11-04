aabe1d877f96f866bd356224c5022dbed56ce1be
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-11-03 22:43:24 -0400

    updated the logout service to fetch the refresh token from the cookie, invalidate the token and delete all the user's tokens from the DB

--- src/main/java/com/meufty/workoutplanner/service/LogoutService.java
+++ src/main/java/com/meufty/workoutplanner/service/LogoutService.java
@@ -1,9 +1,15 @@
 package com.meufty.workoutplanner.service;
 
+import com.meufty.workoutplanner.model.MyUser;
+import com.meufty.workoutplanner.model.MyUserDetails;
+import com.meufty.workoutplanner.model.Token;
 import com.meufty.workoutplanner.repository.TokenRepository;
+import com.meufty.workoutplanner.repository.UserRepository;
+import com.meufty.workoutplanner.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.token.TokenService;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.CookieValue;
@@ -12,6 +18,8 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -19,26 +27,17 @@ public class LogoutService implements LogoutHandler {
 
     @Autowired
     private TokenRepository tokenRepository;
+    @Autowired
+    private JwtUtil jwtUtil;
+    @Autowired
+    private UserRepository userRepository;
     @Override
     public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-        final String authorizationHeader = request.getHeader("Authorization");
-        Cookie[] cookies = request.getCookies();
+        List<Cookie> cookies = Arrays.stream(request.getCookies()).filter(cookie -> cookie.getName().equals("refreshToken")).collect(Collectors.toList());
 
-        String jwt = null;
         String refreshToken = null;
-        if (cookies.length > 0){
-            refreshToken = extractRefreshTokenFromCookie(cookies[0].toString());
-        }
-
-        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-            jwt = authorizationHeader.substring(7);
-        }
-
-        var storedToken = tokenRepository.findByToken(jwt).orElse(null);
-        if (storedToken != null){
-            storedToken.setRevoked(true);
-            storedToken.setExpired(true);
-            tokenRepository.delete(storedToken);
+        if (!cookies.isEmpty()){
+            refreshToken = cookies.get(0).getValue();
         }
 
         var storeRefreshToken = tokenRepository.findByToken(refreshToken).orElse(null);
@@ -47,9 +46,17 @@ public class LogoutService implements LogoutHandler {
             storeRefreshToken.setExpired(true);
             tokenRepository.delete(storeRefreshToken);
         }
-    }
-    
-    public String extractRefreshTokenFromCookie(@CookieValue(name = "refreshToken") String refreshToken){
-        return refreshToken;
+
+        var username = jwtUtil.extractUsername(refreshToken);
+        MyUser myUser = userRepository.findByUsername(username).orElse(null);
+        assert myUser != null;
+        List<Token> tokens = tokenRepository.findAllByUser(myUser);
+
+        //Revoke, expire and delete all jwt tokens the current belong to the user
+        tokens.forEach(token -> {
+            token.setExpired(true);
+            token.setRevoked(true);
+            tokenRepository.delete(token);
+        });
     }
 }

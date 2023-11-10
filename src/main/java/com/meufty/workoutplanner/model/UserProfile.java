7bccaf40210f914fea13977bf11b1a12d06b9ce2
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-11-09 21:01:33 -0500

    Added class for user profile

--- src/main/java/com/meufty/workoutplanner/model/UserProfile.java
+++ src/main/java/com/meufty/workoutplanner/model/UserProfile.java
@@ -1,2 +1,27 @@
-package com.meufty.workoutplanner.model;public class Profile {
+package com.meufty.workoutplanner.model;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "user_profile", uniqueConstraints = {@UniqueConstraint(columnNames = "id")})
+public class UserProfile {
+    @Id
+    @SequenceGenerator(name = "profile_sequence", sequenceName = "profile_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "profile_sequence")
+    private Long id;
+    @JoinColumn(name = "id", nullable = false, table = "app_user")
+    private Long userId;
+    private String firstName;
+    private String lastName;
+    private String preferredName;
+    private String goal;
+    private byte[] profilePhoto;
 }

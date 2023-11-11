11338a77ba178de70140bd68594398fc90490373
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-11-11 17:39:31 -0500

    Added other parameters for enableglobalmethodsecurity - secureEnabled and jsr250enabled properties for allowing roles

--- src/main/java/com/meufty/workoutplanner/security/config/SecurityConfig.java
+++ src/main/java/com/meufty/workoutplanner/security/config/SecurityConfig.java
@@ -34,7 +34,7 @@ import java.util.List;
 @Configuration
 @AllArgsConstructor
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+        @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
 
     private final MyUserDetailsService myUserDetailsService;

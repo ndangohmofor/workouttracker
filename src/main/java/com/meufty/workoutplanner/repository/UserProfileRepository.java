ef326458b615823c7e6703f0fad5cebcd9d1931e
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-11-10 23:11:27 -0500

    Update user profile repository interface to be able to search user profiles by id or role

--- src/main/java/com/meufty/workoutplanner/repository/UserProfileRepository.java
+++ src/main/java/com/meufty/workoutplanner/repository/UserProfileRepository.java
@@ -1,2 +1,14 @@
-package com.meufty.workoutplanner.repository;public interface UserProfileRepository {
+package com.meufty.workoutplanner.repository;
+
+import com.meufty.workoutplanner.model.UserProfile;
+import com.meufty.workoutplanner.model.UserRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
+
+    Optional<UserProfile> findUserProfileByUserId(Long userId);
+    Optional<List<UserProfile>> findUserProfileByUserRole(UserRole role);
 }

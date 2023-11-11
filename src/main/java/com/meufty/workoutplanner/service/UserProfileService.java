86205adeed70e92953e56450e99f3fc68b410c13
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-11-10 23:48:58 -0500

    add method to return list of userprofiles by role in the user profile service

--- src/main/java/com/meufty/workoutplanner/service/UserProfileService.java
+++ src/main/java/com/meufty/workoutplanner/service/UserProfileService.java
@@ -3,10 +3,14 @@ package com.meufty.workoutplanner.service;
 
 import com.meufty.workoutplanner.api.UserProfileRequest;
 import com.meufty.workoutplanner.model.UserProfile;
+import com.meufty.workoutplanner.model.UserRole;
 import com.meufty.workoutplanner.repository.UserProfileRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -14,14 +18,14 @@ import org.springframework.stereotype.Service;
 public class UserProfileService {
     private UserProfileRepository userProfileRepository;
 
-    public void createUserProfile(UserProfile request){
+    public void createUserProfile(UserProfile request) {
         userProfileRepository.save(request);
     }
 
-    public UserProfileRequest fetchUserProfile(Long userId){
+    public UserProfileRequest fetchUserProfile(Long userId) {
         UserProfile profile = userProfileRepository.findUserProfileByUserId(userId).orElse(null);
-        UserProfileRequest profileRequest = null;
-        if (profile != null){
+        UserProfileRequest profileRequest = new UserProfileRequest();
+        if (profile != null) {
             profileRequest.setFirstName(profile.getFirstName());
             profileRequest.setLastName(profile.getLastName());
             profileRequest.setPreferredName(profile.getPreferredName());
@@ -30,5 +34,24 @@ public class UserProfileService {
             profileRequest.setGoal(profile.getGoal());
         }
         return profileRequest;
+    }
+
+    public List<UserProfileRequest> fetchUserProfileByRole(UserRole role) {
+        List<UserProfile> profiles = userProfileRepository.findUserProfileByUserRole(role).orElse(new ArrayList<>());
+        List<UserProfileRequest> userProfiles = new ArrayList<>();
+        if (!profiles.isEmpty()) {
+            profiles.forEach(p -> {
+                UserProfileRequest userProfile = new UserProfileRequest();
+                userProfile.setFirstName(p.getFirstName());
+                userProfile.setLastName(p.getLastName());
+                userProfile.setPreferredName(p.getPreferredName());
+                userProfile.setGoal(p.getGoal());
+                userProfile.setProfilePhoto(p.getProfilePhoto());
+                userProfile.setUserRole(p.getRole());
+                userProfiles.add(userProfile);
+            });
+        }
+        ;
+        return userProfiles;
     }
 }

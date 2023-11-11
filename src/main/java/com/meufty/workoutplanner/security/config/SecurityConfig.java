4a5f5ea61b12633a004dc448daef6ce30563028c
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-11-11 15:46:19 -0500

    Added enableglobalmethodsecurity to be able to use prepost authorization at method levels

--- src/main/java/com/meufty/workoutplanner/security/config/SecurityConfig.java
+++ src/main/java/com/meufty/workoutplanner/security/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -33,6 +34,7 @@ import java.util.List;
 @Configuration
 @AllArgsConstructor
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
     private final MyUserDetailsService myUserDetailsService;

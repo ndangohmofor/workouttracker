ff3f8447f5e070e8f1dda97cca729574365f03e6
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-11-11 16:00:31 -0500

    add method in controller to fetch user profile user the user id, and authorized by role admin and employee

--- src/main/java/com/meufty/workoutplanner/controller/UserProfileController.java
+++ src/main/java/com/meufty/workoutplanner/controller/UserProfileController.java
@@ -6,6 +6,7 @@ import com.meufty.workoutplanner.service.UserProfileService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,6 +24,16 @@ public class UserProfileController {
     private ResponseEntity<?> fetchUserProfile(HttpServletRequest request) {
         try {
             return ResponseEntity.ok(userProfileService.fetchUserProfile(request));
+        } catch (IllegalStateException ex) {
+            return ResponseEntity.status(410).body(ex.getMessage());
+        }
+    }
+
+    @GetMapping()
+    @PreAuthorize("hasRole('ROLE_ADMIN, ROLE_EMPLOYEE')")
+    private ResponseEntity<?> fetchUserProfile(@RequestParam("id") Long userId) {
+        try {
+            return ResponseEntity.ok(userProfileService.fetchUserProfile(userId));
         } catch (IllegalStateException ex) {
             return ResponseEntity.status(410).body(ex.getMessage());
         }

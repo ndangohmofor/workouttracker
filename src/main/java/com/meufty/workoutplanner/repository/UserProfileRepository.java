e85b546bed1a49f59f3acb9145b5407851b29e22
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-11-12 13:49:19 -0500

    Fixed finduserprofilebyrole in the userprofile repository

--- src/main/java/com/meufty/workoutplanner/repository/UserProfileRepository.java
+++ src/main/java/com/meufty/workoutplanner/repository/UserProfileRepository.java
@@ -3,6 +3,7 @@ package com.meufty.workoutplanner.repository;
 import com.meufty.workoutplanner.model.UserProfile;
 import com.meufty.workoutplanner.model.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,5 +11,8 @@ import java.util.Optional;
 public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
 
     Optional<UserProfile> findUserProfileByUserId(Long userId);
+    @Query("""
+            SELECT id, userId, firstName, lastName, preferredName, goal, role, profilePhoto FROM UserProfile U WHERE U.role = :role
+            """)
     Optional<List<UserProfile>> findUserProfileByUserRole(UserRole role);
 }

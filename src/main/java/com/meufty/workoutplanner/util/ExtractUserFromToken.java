d7be59fee228d0825da20f7a587229ca9673d208
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-11-12 13:51:19 -0500

    Created utility method to extract a username from a passed in access token

--- src/main/java/com/meufty/workoutplanner/util/ExtractUserFromToken.java
+++ src/main/java/com/meufty/workoutplanner/util/ExtractUserFromToken.java
@@ -1,2 +1,31 @@
-package com.meufty.workoutplanner.util;public class ExtractUserFromToken {
+package com.meufty.workoutplanner.util;
+
+import com.meufty.workoutplanner.model.MyUser;
+import com.meufty.workoutplanner.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+@AllArgsConstructor
+public class ExtractUserFromToken {
+
+    JwtUtil jwtUtil;
+    UserRepository userRepository;
+    public ResponseEntity<MyUser> extractUserFromToken(HttpServletRequest request){
+        String authorizationHeader = request.getHeader("Authorization");
+
+        String username = null;
+        String jwt = null;
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            jwt = authorizationHeader.substring(7);
+            username = jwtUtil.extractUsername(jwt);
+        }
+        return ResponseEntity.ok(userRepository.findByUsername(username).orElseThrow());
+    }
 }

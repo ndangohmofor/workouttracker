745701c1cccf3c2dcb984008952549c5a9e37012
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-11-16 06:33:23 -0500

    Updated the userprofile repo interface and added method for updating the user profile

--- src/main/java/com/meufty/workoutplanner/repository/UserProfileRepository.java
+++ src/main/java/com/meufty/workoutplanner/repository/UserProfileRepository.java
@@ -3,8 +3,10 @@ package com.meufty.workoutplanner.repository;
 import com.meufty.workoutplanner.model.UserProfile;
 import com.meufty.workoutplanner.model.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,4 +17,19 @@ public interface UserProfileRepository extends JpaRepository<UserProfile, Long> 
             SELECT id, userId, firstName, lastName, preferredName, goal, role, profilePhoto FROM UserProfile U WHERE U.role = :role
             """)
     Optional<List<UserProfile>> findUserProfileByUserRole(UserRole role);
+
+    @Transactional
+    @Modifying
+    @Query("""
+UPDATE UserProfile U SET firstName = :firstName, goal = :goal, lastName = :lastName, preferredName = :preferredName, profilePhoto = :profilePhoto, role = :role WHERE userId = :userId  
+""")
+    int updateUserProfileByUserId(
+            Long userId,
+            String firstName,
+            String goal,
+            String lastName,
+            String preferredName,
+            byte[] profilePhoto,
+            UserRole role
+    );
 }

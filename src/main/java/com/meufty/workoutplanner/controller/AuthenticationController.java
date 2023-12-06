c28bad2f025e5acb748e0359c82f81899fe25a74
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-12-05 23:16:17 -0500

    cleaned up the controller and removed the response entity which is added extra header and body on the response

--- src/main/java/com/meufty/workoutplanner/controller/AuthenticationController.java
+++ src/main/java/com/meufty/workoutplanner/controller/AuthenticationController.java
@@ -1,10 +1,10 @@
 package com.meufty.workoutplanner.controller;
 
 import com.meufty.workoutplanner.api.AuthenticationRequest;
+import com.meufty.workoutplanner.api.AuthenticationResponse;
 import com.meufty.workoutplanner.service.AuthenticationService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,12 +21,12 @@ public class AuthenticationController {
     AuthenticationService service;
 
     @PostMapping(path = "/login")
-    public ResponseEntity<?> createAuthenticationToken(@RequestBody @Valid AuthenticationRequest authenticationRequest, HttpServletResponse response) throws BadCredentialsException {
-       return ResponseEntity.status(200).body(service.authenticate(authenticationRequest, response));
+    public AuthenticationResponse createAuthenticationToken(@RequestBody @Valid AuthenticationRequest authenticationRequest, HttpServletResponse response) throws BadCredentialsException {
+       return service.authenticate(authenticationRequest, response);
     }
 
     @GetMapping(path = "/refreshtoken")
-    public ResponseEntity<?> refreshToken(@CookieValue(name = "refreshToken") String refreshToken) throws Exception {
-        return ResponseEntity.ok(service.refreshToken(refreshToken));
+    public AuthenticationResponse refreshToken(@CookieValue(name = "refreshToken") String refreshToken) throws Exception {
+        return (service.refreshToken(refreshToken));
     }
 }

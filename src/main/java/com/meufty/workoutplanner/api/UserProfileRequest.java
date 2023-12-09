0bf69225822d3b3057671cb9b30ef72ebf4ea6ab
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2023-12-08 21:05:59 -0500

    Added middlename in the userprofile request class

--- src/main/java/com/meufty/workoutplanner/api/UserProfileRequest.java
+++ src/main/java/com/meufty/workoutplanner/api/UserProfileRequest.java
@@ -14,6 +14,7 @@ public class UserProfileRequest {
     private String firstName;
     private String lastName;
     private String preferredName;
+    private String middleName;
     private String username;
     private UserRole userRole;
     private String goal;

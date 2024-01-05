2dfe14b297a12366bdf682dbef4c143f0537bdbe
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-01-04 21:45:48 -0500

    Create boolean that returns if a user is checked in based on the values in checkin and checkout times. Annotated the pojo with lombok and added entity annotations

--- src/main/java/com/meufty/workoutplanner/model/CheckIn.java
+++ src/main/java/com/meufty/workoutplanner/model/CheckIn.java
@@ -2,13 +2,23 @@ package com.meufty.workoutplanner.model;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 
 @Data
+@Entity
+@Table(name = "gym_checkin", uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id"})})
 public class CheckIn {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+    @JoinColumn(name = "user_id")
+    private long userId;
     private LocalDateTime checkInTime;
     private LocalDateTime checkOutTime;
-    private long userId;
+
+    public boolean isCheckedIn(){
+        return getCheckInTime() != null && getCheckOutTime() == null;
+    }
 }

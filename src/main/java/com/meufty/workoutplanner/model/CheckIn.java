0861650a5a6f88dab712f1f7327415fee726db6c
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-01-04 21:33:52 -0500

    use lombok @data to create constructor, getters and setters

--- src/main/java/com/meufty/workoutplanner/model/CheckIn.java
+++ src/main/java/com/meufty/workoutplanner/model/CheckIn.java
@@ -1,2 +1,14 @@
-package com.meufty.workoutplanner.model;public class CheckIn {
+package com.meufty.workoutplanner.model;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+
+@Data
+public class CheckIn {
+    private long id;
+    private LocalDateTime checkInTime;
+    private LocalDateTime checkOutTime;
+    private long userId;
 }

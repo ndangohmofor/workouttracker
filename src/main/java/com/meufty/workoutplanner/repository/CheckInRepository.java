75c623e554d650ab0b9c5eeb8d31d58f95e1fe13
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-01-04 21:56:34 -0500

    Add method in interface to return the checkin times for a particular user

--- src/main/java/com/meufty/workoutplanner/repository/CheckInRepository.java
+++ src/main/java/com/meufty/workoutplanner/repository/CheckInRepository.java
@@ -1,2 +1,11 @@
-package com.meufty.workoutplanner.repository;public interface CheckInRepository {
+package com.meufty.workoutplanner.repository;
+
+import com.meufty.workoutplanner.model.CheckIn;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface CheckInRepository extends JpaRepository<CheckIn, Long> {
+    List<LocalDateTime> getCheckInByIdIs(Long userId);
 }

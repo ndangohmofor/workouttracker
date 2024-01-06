47cb7c5f0e91e4a54d22c903a54e3d82d4ad40bf
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-01-05 23:10:57 -0500

    Add Checkin Service to return the list of Checkins of a particular user

--- src/main/java/com/meufty/workoutplanner/service/CheckInService.java
+++ src/main/java/com/meufty/workoutplanner/service/CheckInService.java
@@ -1,2 +1,24 @@
-package com.meufty.workoutplanner.service;public class CheckInService {
+package com.meufty.workoutplanner.service;
+
+import com.meufty.workoutplanner.model.CheckIn;
+import com.meufty.workoutplanner.model.MyUser;
+import com.meufty.workoutplanner.repository.CheckInRepository;
+import com.meufty.workoutplanner.util.JwtUtil;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@AllArgsConstructor
+@Service
+public class CheckInService {
+    private CheckInRepository checkInRepository;
+    private JwtUtil jwtUtil;
+
+    public List<CheckIn> getCheckins(HttpServletRequest request){
+        MyUser user = jwtUtil.getUserFromHttpRequest(request);
+        return checkInRepository.getCheckInById(user.getId());
+    }
 }

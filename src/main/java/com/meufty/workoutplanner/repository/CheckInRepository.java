ed03c7b7a7dc5cdf343409bbe8cc9a8d1547020d
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-01-05 23:07:54 -0500

    Updated the Checkin repository to return the correct type

--- src/main/java/com/meufty/workoutplanner/repository/CheckInRepository.java
+++ src/main/java/com/meufty/workoutplanner/repository/CheckInRepository.java
@@ -2,10 +2,12 @@ package com.meufty.workoutplanner.repository;
 
 import com.meufty.workoutplanner.model.CheckIn;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface CheckInRepository extends JpaRepository<CheckIn, Long> {
-    List<LocalDateTime> getCheckInById(Long userId);
+    List<CheckIn> getCheckInById(Long userId);
 }

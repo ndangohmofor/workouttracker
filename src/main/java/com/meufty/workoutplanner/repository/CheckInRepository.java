d7e51624e6ce10dce23bc8fbb56bc74b26366577
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-01-05 23:41:04 -0500

    Add method to the repo interface to find the current checkin for a user

--- src/main/java/com/meufty/workoutplanner/repository/CheckInRepository.java
+++ src/main/java/com/meufty/workoutplanner/repository/CheckInRepository.java
@@ -2,6 +2,7 @@ package com.meufty.workoutplanner.repository;
 
 import com.meufty.workoutplanner.model.CheckIn;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -10,4 +11,9 @@ import java.util.List;
 @Repository
 public interface CheckInRepository extends JpaRepository<CheckIn, Long> {
     List<CheckIn> getCheckInById(Long userId);
+
+    @Query("""
+            SELECT C FROM CheckIn C WHERE C.userId = :userId AND C.checkOutTime = NULL 
+            """)
+    CheckIn getCurrentCheckInById(Long userId);
 }

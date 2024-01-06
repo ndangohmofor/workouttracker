3851af96f13ca00cf4ffc42e2fa6c6f02cabebfd
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-01-05 23:48:27 -0500

    Add security to the checkin controller methods for particular roles to be able to access

--- src/main/java/com/meufty/workoutplanner/controller/CheckinController.java
+++ src/main/java/com/meufty/workoutplanner/controller/CheckinController.java
@@ -3,6 +3,7 @@ package com.meufty.workoutplanner.controller;
 import com.meufty.workoutplanner.model.CheckIn;
 import com.meufty.workoutplanner.service.CheckInService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,16 +17,19 @@ public class CheckinController {
     private CheckInService checkInService;
 
     @GetMapping(path = "/usercheckins")
+    @Secured({"ROLE_USER"})
     public List<CheckIn> getUserCheckIns(HttpServletRequest request){
         return checkInService.getCheckins(request);
     }
 
     @PostMapping(path = "usercheckin")
+    @Secured({"ROLE_USER"})
     public CheckIn addUserCheckIn(HttpServletRequest request){
         return checkInService.addCheckin(request);
     }
 
     @PostMapping(path = "usercheckout")
+    @Secured({"ROLE_USER"})
     public CheckIn addUserCheckout(HttpServletRequest request){
         return checkInService.addCheckout(request);
     }

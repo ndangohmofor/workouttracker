a52055ad222f86e7bdff1d57b9cdc5905c416e26
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-01-11 06:10:37 -0500

    Maximize imports

--- src/main/java/com/meufty/workoutplanner/repository/CheckInRepository.java
+++ src/main/java/com/meufty/workoutplanner/repository/CheckInRepository.java
@@ -1,7 +1,6 @@
 package com.meufty.workoutplanner.repository;
 
 import com.meufty.workoutplanner.model.CheckIn;
-import com.meufty.workoutplanner.model.MyUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;

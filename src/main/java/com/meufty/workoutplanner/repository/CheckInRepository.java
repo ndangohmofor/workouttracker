3750fcacea8e97f9145d75379398b44444dd4edd
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-01-11 06:09:45 -0500

    Show time since last visit rather than first

--- src/main/java/com/meufty/workoutplanner/repository/CheckInRepository.java
+++ src/main/java/com/meufty/workoutplanner/repository/CheckInRepository.java
@@ -18,7 +18,7 @@ public interface CheckInRepository extends JpaRepository<CheckIn, Long> {
             """)
     CheckIn getCurrentCheckInById(Long userId);
     @Query("""
-            SELECT min(c.checkInTime) FROM CheckIn c where c.userId = :userId
+            SELECT max(c.checkOutTime) FROM CheckIn c where c.userId = :userId
             """)
     LocalDateTime getFirstCheckinDateById(Long userId);
 }

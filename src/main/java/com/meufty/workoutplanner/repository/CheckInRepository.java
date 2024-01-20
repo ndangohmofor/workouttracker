0eedd18f50cb7e7956d019515df3cb34717f1911
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-01-19 21:10:29 -0500

    Fixed the native query order

--- src/main/java/com/meufty/workoutplanner/repository/CheckInRepository.java
+++ src/main/java/com/meufty/workoutplanner/repository/CheckInRepository.java
@@ -16,7 +16,7 @@ public interface CheckInRepository extends JpaRepository<CheckIn, Long> {
             SELECT c FROM CheckIn c WHERE c.userId = :userId AND c.checkOutTime = NULL
             """)
     CheckIn getCurrentCheckInById(Long userId);
-    @Query(value = "SELECT * FROM gym_checkin c where c.user_id = :userId order by c.check_out_time limit 1", nativeQuery = true)
+    @Query(value = "SELECT * FROM gym_checkin c where c.user_id = :userId order by c.check_out_time desc limit 1", nativeQuery = true)
     CheckIn getLastWorkoutDateById(Long userId);
 
     List<CheckIn> getCheckInByUserId(Long userId);
